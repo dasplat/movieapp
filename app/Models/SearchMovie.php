1be39b86c43f24bf5a1bef755895b66c2b8b7bea
robin ramaekers <robin.ramaekers2@gmail.com>
2025-09-02 17:59:45 +0200

    Bug fix: Release date undefined

--- app/Models/SearchMovie.php
+++ app/Models/SearchMovie.php
@@ -16,31 +16,37 @@ class SearchMovie extends Model
     public static function setVar($value)
     {
         self::$var = $value;
-		return self::query();
+        return self::query();
     }
 
     public function getRows(): array
     {
         $response = Http::asJson()
-        ->acceptJson()
-        ->get('https://api.themoviedb.org/3/search/movie', [
-            'api_key' => '045a227c65ecd494e11b32ee34ec4b05',
-            'query' => self::$var ?? '',
-        ]);
-
-        // dd($response['results']);
+            ->acceptJson()
+            ->get('https://api.themoviedb.org/3/search/movie', [
+                'api_key' => '045a227c65ecd494e11b32ee34ec4b05',
+                'query' => self::$var ?? '',
+            ]);
 
         $movies = Arr::map($response['results'], function ($item) {
             $posterPath = isset($item['poster_path'])
                 ? 'https://image.tmdb.org/t/p/w500' . $item['poster_path']
-                : 'path/to/default/image.jpg'; // Replace with your default image URL
+                : 'path/to/default/image.jpg';
+
+            $releaseDate = isset($item['release_date']) ?
+                $item['release_date'] :
+                null;
+
+            $voteAverage = isset($item['vote_average']) ?
+                $item['vote_average'] :
+                null;
 
             return [
-                'title' => $item['title'], // Directly assign the title
-                'overview' => $item['overview'], // Directly assign the overview
-                'poster_path' => $posterPath, // Use the processed poster_path
-                'release_date' => $item['release_date'], // Directly assign release_date
-                'vote_average' => $item['vote_average'], // Directly assign vote_average
+                'title' => $item['title'],
+                'overview' => $item['overview'],
+                'poster_path' => $posterPath,
+                'release_date' => $releaseDate,
+                'vote_average' => $voteAverage,
             ];
         });
 
@@ -48,6 +54,5 @@ class SearchMovie extends Model
         return array_filter($movies, function ($item) {
             return isset($item['title'], $item['overview'], $item['poster_path'], $item['release_date'], $item['vote_average']);
         });
-
     }
 }
